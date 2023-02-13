85c89ecedf146800f7721a1f354fdde5d84ca02c
Tanvi Agarwal <45534001+tanviagwl98@users.noreply.github.com>
2023-02-13 18:07:18 +0530

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyBh724vdsU4vjXtUqZjkF2mMJwYqoVB2lQ";
+const GOOGLE_API_KEY = "AIzaSyBleSUoWrQ3sxMQuBrmayTgsIlBsCAkqWQ";
 
 export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
